b36e8aa55ab42a195065e523e6395ac63500374e
godwins3 <praisegodwins4@gmail.com>
2024-02-29 16:31:25 +0300

    create api endpoint

--- server.py
+++ server.py
@@ -40,22 +40,23 @@ def upload_form():
     return render_template('upload_form.html')
 
 # Route to handle the file upload and question submission
-@app.route('/upload', methods=['POST'])
+@app.route('/chatbot', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
+        msg_received = request.get_json()
         # Check if the POST request has a file part
-        if 'file' not in request.files:
-            return 'No file part'
+        # if 'file' not in request.files:
+        #     return 'No file part'
         
-        file = request.files['file']
+        # file = request.files['file']
 
         # If the user does not select a file, the browser submits an empty file without a filename
-        if file.filename == '':
-            return 'No selected file'
+        # if file.filename == '':
+        #     return 'No selected file'
 
         # Save the uploaded PDF file
-        pdf_path = 'uploads/' + file.filename
-        file.save(pdf_path)
+        pdf_path = 'uploads/faqs.pdf'
+        # file.save(pdf_path)
 
         # Upload PDF to chat PDF API
         api_key = 'sec_krPXTZLBgir0WGcHMq9L1hTriluOEHJq'
@@ -65,22 +66,52 @@ def upload_file():
             return 'Failed to upload PDF'
 
         # Get user's question from the form
-        question = request.form['question']
+        question = msg_received['question']
 
         # Send message to PDF chat
         result = send_message_to_pdf_chat(source_id, question, api_key)
 
+        
         if result is not None:
-            return render_template('result.html', question=question, answer=result)
+            res = {
+                "answer": result,
+                "statusCode": 200
+            }
+            # return render_template('result.html', question=question, answer=result)
+            return res
         else:
-            return 'Failed to send message to PDF chat'
+            res = {
+                "message": "Error sending message to pdf",
+                "statusCode": 401
+            }
+            return res
 
-@app.route('/incoming-messages', methods=['POST'])
-def incoming_messages():
-   data = request.get_json(force=True)
-   print(f'Incoming message...\n ${data}')
-   
-   return Response(status=200)
+# @app.route('/incoming-messages', methods=['POST'])
+# def incoming_messages():
+#     data = request.get_json(force=True)
+#     print(f'Incoming message...\n ${data}')
+#     # Upload PDF to chat PDF API
+#     # Save the uploaded PDF file
+#     pdf_path = 'uploads/' + file.filename
+#     file.save(pdf_path)
+#     api_key = 'sec_krPXTZLBgir0WGcHMq9L1hTriluOEHJq'
+#     source_id = upload_pdf(pdf_path, api_key)
+
+#     if source_id is None:
+#         return 'Failed to upload PDF'
+
+#     # Get user's question from the form
+#     question = request.form['question']
+
+#     # Send message to PDF chat
+#     result = send_message_to_pdf_chat(source_id, question, api_key)
+    
+#     res ={
+#         "result": result,
+
+#     }
+
+#     return Response(status=200)
 
 @app.route('/delivery-reports', methods=['POST'])
 def delivery_reports():
